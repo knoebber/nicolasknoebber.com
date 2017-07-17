e1ce9e024bf547c2214bfd97c8730f6b00721c48
Nicolas Knoebber <knoebber@gmail.com>
2017-07-17 01:18:50 +0000

    get POST

---
+++ trees/make_tree.php
@@ -0,0 +1,11 @@
+<?php
+
+$length   = $_POST['length']
+$angle    = $_POST['angle']
+$branches = $_POST['branches']
+$depth    = $_POST['depth']
+var_dump($length)
+var_dump($angle)
+var_dump($branches)
+var_dump($depth)
+?>
