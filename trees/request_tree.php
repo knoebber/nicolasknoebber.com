c19c7cf5139b8eba2c04687d42013feeaffa77b0
Nicolas Knoebber <knoebber@gmail.com>
2017-07-23 04:04:12 +0000

    check parameters more intelligently

--- trees/request_tree.php
+++ trees/request_tree.php
@@ -2,13 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$length   = isset($_POST['length'])? $_POST['length']:false;
-$angle    = isset($_POST['angle'])? $_POST['angle']:false;
-$depth    = isset($_POST['depth'])? $_POST['depth']:false;
-$branches = isset($_POST['branches'])? $_POST['branches']:false;
-if($length&&$angle&&$depth&&$branches) {//TODO make sure this doesn't let 0s or negatives by
-  if($depth * $branches < 30){ //so we don't try to generate something to complex
+function checkVar($param) {
+  return (isset($param) && is_numeric($param) && $param > 0);
+}
 
+$length   = checkVar($_POST['length'])? $_POST['length']:false;
+$angle    = checkVar($_POST['angle'])? $_POST['angle']:false;
+$depth    = checkVar($_POST['depth'])? $_POST['depth']:false;
+$branches = checkVar($_POST['branches'])? $_POST['branches']:false;
+
+if($length&&$angle&&$depth&&$branches) {
+  if($depth * $branches <= 30){ //so we don't try to generate something to complex
     $depth    = round($depth);
     $angle    = round($angle);
     $branches = round($branches);
@@ -16,18 +20,20 @@ if($length&&$angle&&$depth&&$branches) {//TODO make sure this doesn't let 0s or 
     $x = 900;
     $y = 900;
     $command = "/var/www/trees/tree.py $depth $length $angle $branches $x $y 2>&1";
-    $file = exec($command);//TODO fix permissions on python file
+    $file = exec($command);
     if($file != '') {
       echo json_encode(array('status'=>'success','message'=>'tree was generated','file'=>"$file"));
-      }
-      else {json_encode(array('status'=>'error','message'=>'there was an error with the script'));}
+    }
+    else {
+      echo json_encode(array('status'=>'error','message'=>'there was an error with the script'));
+    }
   }//if
   else {
     echo json_encode(array('status'=>'error','message'=>'tree is too complex'));
-  }//else
+  }
 }//if
 else {
     echo json_encode(array('status'=>'error','message'=>'invalid parameters'));
-}//else
+}
 
 ?>
