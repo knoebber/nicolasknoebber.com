eacc72e86786f37dfa536c6fe14779df1a0dcb10
Nicolas Knoebber <knoebber@gmail.com>
2018-07-19 20:54:35 -0700

    finish basic post adding

--- posts/create_post.py
+++ posts/create_post.py
@@ -1,20 +1,45 @@
+#!/usr/bin/python3
+from markdown import markdown
+from sys      import argv
+
 """
 adds a new entry to blog.html
+returns True if blog.html was written
 """
-def add_entry_to_list() :
-  md     = open('4.md')
+def add_entry_to_list(post_num) :
+  post_num = str(post_num)
+  md     = open(post_num+'.md')
   header = md.readline()[4:-1] #slice omits the beginning hashes and trailing \n
   md.close()
   html = open('../blog.html')
   lines = html.readlines()
-  for i in range(0,len(lines)) :
-    if lines[i] == '</ol>\n' :
-      lines[i -1] = ' <li><a href="posts/4.html">'+header+'</a></li>\n'
-      print('made it')
-      break
   html.close()
+  new_element = ' <li><a href="posts/'+post_num+'.html">'+header+'</a></li>\n'
+  for i in range(0,len(lines)) :
+    if lines[i] == new_element:
+      print('list item already exists')
+      return
+    if lines[i] == '</ol>\n' :
+      lines.insert(i,new_element)
+      break
   html = open('../blog.html','wt')
   html.writelines(lines)
   html.close()
+  return True
 
-add_entry_to_list()
+def add_entry(post_num) :
+  try :
+    md = open(post_num+'.md')
+  except :
+    print(post_num+'.md'+' does not exist!')
+    return
+  html = markdown(md.read())
+  md.close()
+  post = open(post_num+'.html','wt')
+  post.write(html)
+  post.close()
+  add_entry_to_list(post_num)
+
+if __name__ == '__main__' :
+  post_num = argv[1]
+  add_entry(post_num)
