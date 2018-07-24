34ab0c503dcda5f09f60a85fc0f079b1f33b8002
Nicolas Knoebber <knoebber@gmail.com>
2018-07-23 17:53:54 -0700

    add header to new posts

--- posts/create_post.py
+++ posts/create_post.py
@@ -3,30 +3,32 @@ from markdown import markdown
 from sys      import argv
 
 """
-adds a new <li> element to the <ol> in blog.html
-this always adds it to the end
+adds a new <tr> element to blog.html
+new row will always be the first to keep reverse chronoligical order
 """
 def add_entry_to_list(post_num) :
   post_num = str(post_num)
   md     = open(post_num+'.md')
   header = md.readline()[3:-1] #slice omits the beginning hashes and trailing \n
+  date   = md.readline()[5:-1]
   md.close()
   html = open('../blog.html')
   lines = html.readlines()
   html.close()
-  new_element = ' <li><a href="posts/'+post_num+'.html">'+header+'</a></li>\n'
-  for i in range(0,len(lines)) :
-    if lines[i] == new_element:
+  new_element = '<tr><td><a href="posts/'+post_num+'.html">'+header+'</a></td><td>'+date+'</td></tr>\n'
+  i = len(lines) - 1
+  while i > 0 :
+    if lines[i].strip() == new_element.strip() :
       print('list item already exists')
       return
-    if lines[i] == '</ol>\n' :
-      lines.insert(i,new_element)
-      break
-  html = open('../blog.html','wt')
-  html.writelines(lines)
-  html.close()
-  print('new list item created')
-  return True
+    if lines[i].strip() == '<tbody>' :
+      lines.insert(i+1,' '*4+new_element) #indent new tag properly and add to file
+      print('new list item created')
+      html = open('../blog.html','wt')
+      html.writelines(lines)
+      html.close()
+      return
+    i -= 1
 
 """
 reads from the markdown file (post_num).md and writes (post_num).html
@@ -37,8 +39,12 @@ def add_entry(post_num) :
   except :
     print(post_num+'.md'+' does not exist!')
     return
+  h = open('header.html')
+  header = h.read()
+  h.close()
   html = markdown(md.read())
   html = '<LINK REL=StyleSheet HREF="style.css" TYPE="text/css">\n' + html
+  html = header + html
   md.close()
   post = open(post_num+'.html','wt')
   post.write(html)
