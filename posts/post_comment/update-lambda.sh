d4238ca6d247f1e4ecbc626e573a136d49cc7d95
Nicolas Knoebber <knoebber@gmail.com>
2018-10-14 16:59:15 -0500

    restructure

--- posts/post_comment/update-lambda.sh
+++ posts/post_comment/update-lambda.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-cd $HOME/code/personal-website/lambda/post_comment
 zip -r post_comment.zip index.js
 aws lambda update-function-code --function-name post_comment --zip-file fileb://post_comment.zip
 rm post_comment.zip
