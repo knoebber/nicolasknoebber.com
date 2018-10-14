cf476a9b43338064155d408459e417a89f4024d5
Nicolas Knoebber <knoebber@gmail.com>
2018-10-14 01:07:37 -0500

    use python json tool, add commandline arg for curl option

--- posts/scripts/api-test.sh
+++ posts/scripts/api-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 KEY=$(cat api-key)
-curl -H \
+curl $1 -H \
   "x-api-key: $KEY" \
   -H "Content-Type: application/json" \
   -d @example_request.json \
-  https://cx00ooxpol.execute-api.us-west-2.amazonaws.com/default/post_comment
+  https://cx00ooxpol.execute-api.us-west-2.amazonaws.com/default/post_comment | python -mjson.tool
