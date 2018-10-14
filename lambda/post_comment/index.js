2e6c658eea21a44b7f57460b33378c52ea70f559
Nicolas Knoebber <knoebber@gmail.com>
2018-10-14 16:32:49 -0500

    refactor

--- lambda/post_comment/index.js
+++ lambda/post_comment/index.js
@@ -3,42 +3,37 @@ const dynamo = new AWS.DynamoDB();
 const table = "comment";
 
 exports.handler = (event, context, callback) => {
-  const body = JSON.parse(event.body)
-  const {comment_name,comment_body:comment_body,number} = body
-  var code
-  var message = "";
+  const body = JSON.parse(event.body);
+  const {post_number,comment_name,comment_body:comment_body} = body;
+  const now = new Date().getTime();
   const dynamo_request = {
     Item: {
-     "comment_id": {
-       S: "test"
+      "time_stamp": {
+        S: now.toString()
       },
-     "comment_body": {
-       S: comment_body
+      "post_number" : {
+        S:post_number.toString()
       },
-     "comment_name": {
-       S: comment_name
+      "comment_body": {
+        S: comment_body
+      },
+      "comment_name": {
+        S: comment_name
       }
     },
     ReturnConsumedCapacity: "TOTAL",
     TableName: table
-  }
+  };
+  const respond = (code,message) => {
+    callback(null, {
+      statusCode: code,
+      body: JSON.stringify(message)
+    });
+  };
   dynamo.putItem(dynamo_request, (err, data) => {
-    if (!err){
-      code = 200
-      message = `adding: comment_name:${comment_name},comment_body:${comment_body},number:${number}\n`
-      // TODO use context to exit and set body here inside async request
-    }
-    else {
-      code = 500
-      message = `an error occurred: ${err}\n`
-    }
+    if (!err)
+      respond(200,`name:${comment_name},body:${comment_body},for post:${post_number}`);
+    else
+      respond(500,`an error occured ${err}`);
   });
-
-  const response = {
-    statusCode: code,
-    body: JSON.stringify(message)
-  }
-
-  callback(null, response)
 };
-
