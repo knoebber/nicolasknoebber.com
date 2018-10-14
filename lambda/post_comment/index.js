3414d708ad104504f5eab3caf1977f24f5bf2e43
Nicolas Knoebber <knoebber@gmail.com>
2018-10-14 01:07:49 -0500

    add request to dynamo db

--- lambda/post_comment/index.js
+++ lambda/post_comment/index.js
@@ -1,10 +1,44 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB();
+const table = "comment";
+
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body)
-  const {comment_name,comment_body:comment_body,number} = body;
-  const message = `comment_name:${comment_name},comment_body:${comment_body},number:${number}`
+  const {comment_name,comment_body:comment_body,number} = body
+  var code
+  var message = "";
+  const dynamo_request = {
+    Item: {
+     "comment_id": {
+       S: "test"
+      },
+     "comment_body": {
+       S: comment_body
+      },
+     "comment_name": {
+       S: comment_name
+      }
+    },
+    ReturnConsumedCapacity: "TOTAL",
+    TableName: table
+  }
+  dynamo.putItem(dynamo_request, (err, data) => {
+    if (!err){
+      code = 200
+      message = `adding: comment_name:${comment_name},comment_body:${comment_body},number:${number}\n`
+      // TODO use context to exit and set body here inside async request
+    }
+    else {
+      code = 500
+      message = `an error occurred: ${err}\n`
+    }
+  });
+
   const response = {
-    statusCode: 200,
+    statusCode: code,
     body: JSON.stringify(message)
-  };
-  callback(null, response);
+  }
+
+  callback(null, response)
 };
+
