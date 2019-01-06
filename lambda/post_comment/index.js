9a09f7883c93b2131ebe85640454c2c52c2e99a9
Nicolas Knoebber <knoebber@gmail.com>
2019-01-05 23:48:13 -0600

    better api responses

--- lambda/post_comment/index.js
+++ lambda/post_comment/index.js
@@ -3,42 +3,48 @@ const dynamo = new AWS.DynamoDB();
 const table = "comment";
 
 exports.handler = (event, context, callback) => {
-  const {post_number,comment_name,comment_body:comment_body} = JSON.parse(event.body);
+  const {
+    postNumber,
+    commentName,
+    commentBody
+  } = JSON.parse(event.body);
 
-  const dynamo_request = {
-    Item: {
-      "time_stamp": {
-        N: new Date().getTime().toString()
-      },
-      "post_number" : {
-        N:post_number.toString()
-      },
-      "comment_body": {
-        S: comment_body
-      },
-      "comment_name": {
-        S: comment_name
-      }
+  const newItem = {
+    "time_stamp": {
+      N: new Date().getTime().toString()
     },
+    "post_number" : {
+      N:postNumber.toString()
+    },
+    "comment_body": {
+      S: commentBody
+    },
+    "comment_name": {
+      S: commentName
+    }
+  };
+
+  const dynamoRequest = {
+    Item: newItem,
     ReturnConsumedCapacity: "TOTAL",
     TableName: table
   };
 
-  const respond = (code,message) => {
-    callback(null, {
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-      },
-      statusCode: code,
-      body: JSON.stringify({
-        "status"  : code,
-        "message" : message
-      })
-    });
-  };
-
-  dynamo.putItem(dynamo_request, (err, data) => {
-    if (!err) respond(200, `added comment for post ${post_number}! name:${comment_name}, body:${comment_body}`);
-    else      respond(500,`an error occured ${err}`);
+  dynamo.putItem(dynamoRequest, (err, data) => {
+    if (!err) respond(callback, 200, newItem);
+    else      respond(callback, 500,`an error occured: ${err}`);
   });
 };
+
+function respond(callback, code, body){
+  const response = {
+    statusCode:code,
+    headers:{
+      "Access-Control-Allow-Origin" : "*"
+    }
+  };
+
+  if (code == 200) response.body = JSON.stringify(body);
+  else             response.body = JSON.stringify({'message':body});
+  callback(null, response);
+}
