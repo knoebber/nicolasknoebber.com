60f98d9fb32c781296e0b2307b5d47be9e12c3b4
Nicolas Knoebber <knoebber@gmail.com>
2019-01-12 14:13:48 -0600

    use absolute paths, clean up docs

--- posts/scripts/create_post.py
+++ posts/scripts/create_post.py
@@ -2,31 +2,47 @@ from markdown import markdown
 from sys      import argv
 
 """
+global variables for paths
+"""
+website_dir = os.environ['HOME'] + '/projects/personal-website'
+posts_dir =  website_dir + '/posts'
+partial_dir = posts_dir + '/partial'
+markdown_dir = posts_dir + '/markdown'
+
+
+"""
 adds a new <tr> element to blog.html
-new row will always be the first to keep reverse chronoligical order
+new row will always be the first, to keep reverse chronological order
 """
 def add_entry_to_list(post_num) :
-  md = open('../markdown/'+post_num+'.md')
+  # open the markdown file and get the date and title
+  md = open(markdown_dir+'/'post_num+'.md')
   header = md.readline()[3:-1] # slice omits the beginning hashes and trailing \n
   date   = md.readline()[5:-1]
   md.close()
-  html = open('../../blog.html')
+
+  # open the blog file and puts its contents into a list of lines
+  html = open(website_dir+'/blog.html')
   lines = html.readlines()
   html.close()
+
+  # the new table row to be inserted
   new_element = '<tr><td><a href="posts/'+post_num+'.html">'+header+'</a></td><td>'+date+'</td></tr>\n'
+
+  # check if the table row already exists by iterating backwards through the lines
   i = len(lines) - 1
   while i > 0 :
     if lines[i].strip() == new_element.strip() :
       print('list item already exists')
       return
-    if lines[i].strip() == '<tbody>' :
-      lines.insert(i+1,' '*4+new_element) # indent new tag properly and add to file
-      print('new list item created')
-      html = open('../../blog.html','wt')
-      html.writelines(lines)
-      html.close()
-      return
-    i -= 1
+  if lines[i].strip() == '<tbody>' :
+    lines.insert(i+1,(' '*6)+new_element) # indent new tag properly and add to file
+    html = open(posts_dir,'wt')
+    html.writelines(lines)
+    html.close()
+    print('new list item created')
+    return
+  i -= 1
 
 """
 reads from the markdown file (post_num).md and writes (post_num).html
@@ -34,18 +50,18 @@ reads from the markdown file (post_num).md and writes (post_num).html
 def add_entry(post_num) :
   post_num = str(post_num)
   try :
-    md = open('../markdown/'+post_num+'.md')
+    md = open(markdown_dir+post_num+'.md')
   except :
-    print(post_num+'.md'+' does not exist!')
     return False
 
   # read header and footer
-  h = open('../partial/header.html')
+  h = open(partial_dir+'/header.html')
   header = h.read()
   h.close()
-  f = open('../partial/footer.html')
+  f = open(partial_dir+'/footer.html')
   footer = f.read()
   f.close()
+
   # add a post number variable to the footer so it knows how to save comments
   footer = "<script> const postNum = "+post_num + ";</script>" + footer;
 
@@ -55,7 +71,7 @@ def add_entry(post_num) :
   md.close()
 
   # write html file
-  post = open('../'+post_num+'.html','wt')
+  post = open(posts_dir+'/'+post_num+'.html','wt')
   post.write(html)
   post.close()
   print('post updated')
@@ -66,7 +82,6 @@ if __name__ == '__main__' :
   post_num = argv[1]
   if post_num == 'all' :
     n = 0
-    while add_entry(n) :
-      n += 1
+    while add_entry(n) : n += 1
   else :
     add_entry(post_num)
