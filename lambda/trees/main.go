2d66be46148187ed947dea688b1b9373e95a35ee
Nicolas Knoebber <knoebber@gmail.com>
2019-01-25 21:04:52 -0600

    add new files

---
+++ lambda/trees/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+//	"fmt"
+// "github.com/aws/aws-lambda-go/lambda"
+// "github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+/*
+// Request is the structure that HandleRequest expects to receive.
+type Request struct {
+	Message string `json:"message"`
+}
+
+// HandleRequest processes a lambda request.
+func HandleRequest(r Request) (string, error) {
+	return fmt.Sprintf("It worked %s!", r.Message), nil
+}
+*/
+
+func main() {
+	draw()
+	//	lambda.Start(HandleRequest)
+}
