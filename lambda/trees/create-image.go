d2f97efb10795dc2515992cf092b6af154983ef3
Nicolas Knoebber <knoebber@gmail.com>
2019-02-09 22:05:06 -0600

    Refactor

--- lambda/trees/create-image.go
+++ lambda/trees/create-image.go
@@ -1,68 +1,52 @@
 package main
 
 import (
-  "bytes"
-  "github.com/fogleman/gg"
-  "fmt"
-  "math"
+	"bytes"
+	"fmt"
+	"github.com/fogleman/gg"
+	"math"
 )
 
 const (
-  width = 400
-  height = 400
+	width  = 400
+	height = 400
 )
 
-func polarLine(c *gg.Context, x0 ,y0, length, degrees float64)(x1,y1 float64) {
-  theta := gg.Radians(degrees)
-  x1 = length*(math.Cos(theta))+x0
-  y1 = length*(math.Sin(theta))+y0
-  // fmt.Printf("(%f, %f),(%f,%f)\n",x0,y0,x1,y1)
-  c.DrawLine(x0,y0,x1,y1)
-  c.Stroke()
-  return
+func polarLine(c *gg.Context, x0, y0, length, degrees float64) (x1, y1 float64) {
+	theta := gg.Radians(degrees)
+	x1 = length*(math.Cos(theta)) + x0
+	y1 = length*(math.Sin(theta)) + y0
+	c.DrawLine(x0, y0, x1, y1)
+	c.Stroke()
+	return
 }
 
-func drawTree(c *gg.Context,lineWidth, x0, y0, length, degrees float64, param treeParam) {
-  if lineWidth < 1 || x0 < 1 || y0 < 1 || length < 1 {
-    return
-  }
-  var x1 float64
-  var y1 float64
+func tree(c *gg.Context, lineWidth, x0, y0, length, degrees float64, p TreeParam) {
+	if lineWidth < 1 || x0 < 1 || y0 < 1 || x0 > width || y0 > height || length < 1 {
+		return
+	}
 
-  c.SetLineWidth(lineWidth)
-  lineWidth -= 2
-  x1, y1 = polarLine(c,x0,y0,length,degrees)
-  drawTree(c,lineWidth,x1,y1,length-param.leftLengthOffset, degrees-param.leftAngleOffset,param)
-  drawTree(c,lineWidth,x1,y1,length-param.rightLengthOffset, degrees + param.rightAngleOffset,param)
+	c.SetLineWidth(lineWidth)
+	lineWidth -= 2
+	x1, y1 := polarLine(c, x0, y0, length, degrees)
+	tree(c, lineWidth, x1, y1, length-p.LeftLength, degrees-p.LeftAngle, p)
+	tree(c, lineWidth, x1, y1, length-p.RightLength, degrees+p.RightAngle, p)
 }
 
-type treeParam struct {
-  leftLengthOffset float64
-  leftAngleOffset float64
-  rightLengthOffset float64
-  rightAngleOffset float64
-}
+func createTree(p TreeParam) (buffer *bytes.Buffer, err error) {
 
-func draw() (buffer *bytes.Buffer, err error) {
 	c := gg.NewContext(width, height)
 	c.SetRGB(0, 0, 0)
-  param := treeParam {
-    leftAngleOffset: 15,
-    leftLengthOffset:1,
-    rightLengthOffset:1,
-    rightAngleOffset:45,
-  }
+	tree(c, 15, width/2, height, 100, 270, p)
+	// Save the image to the disk if testing locally.
+	if dev {
+		c.SavePNG("test.png")
+	}
 
-  drawTree(c,10,width/2, height,100,270,param)
-  // If testing locally, save the image to the disk.
-  if dev {
-    c.SavePNG("test.png")
-  }
-
-  // Write the bytes from the image in the context to a buffer.
-  buffer = new(bytes.Buffer)
-  if err = c.EncodePNG(buffer); err != nil {
-    fmt.Printf("failed to encode png %s",err.Error())
-  }
-  return
+	// Write the bytes from the image in the context to a buffer.
+	buffer = new(bytes.Buffer)
+	if err = c.EncodePNG(buffer); err != nil {
+		fmt.Printf("failed to encode png %s", err.Error())
+	}
+	return
 }
--- lambda/trees/main.go
+++ lambda/trees/main.go
@@ -1,53 +1,58 @@
 package main
 
 import (
- "bytes"
- "fmt"
- "github.com/aws/aws-sdk-go/aws"
- "github.com/aws/aws-lambda-go/lambda"
- "github.com/aws/aws-sdk-go/service/s3"
- "github.com/aws/aws-sdk-go/aws/session"
+	"bytes"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// Request is the structure that HandleRequest expects to receive.
-type Request struct {
-	Message string `json:"message"`
+// TreeParam is the structure that HandleRequest expects to receive.
+type TreeParam struct {
+	LeftLength  float64 `json:"leftLength"`
+	LeftAngle   float64 `json:"leftAngle"`
+	RightLength float64 `json:"rightLength"`
+	RightAngle  float64 `json:"rightAngle"`
 }
 
 // HandleRequest processes a lambda request.
-func HandleRequest(r Request) (string, error) {
-  buffer, err := draw()
-  if err != nil {
-    return "", err
-  }
+func HandleRequest(p TreeParam) (string, error) {
+	buffer, err := createTree(p)
+	if err != nil {
+		return "", err
+	}
 
-  // Create a S3 client
-  session := session.Must(session.NewSession())
-  svc := s3.New(session)
+	// Create a S3 client
+	session := session.Must(session.NewSession())
+	svc := s3.New(session)
 
-  reader := bytes.NewReader(buffer.Bytes())
-  fmt.Printf("%d bytes",reader.Len())
+	reader := bytes.NewReader(buffer.Bytes())
+	fmt.Printf("%d bytes", reader.Len())
 
-  putInput := s3.PutObjectInput{
-    Bucket: aws.String("nicolasknoebber.com"),
-    Body:   reader,
-    Key:    aws.String("test_upload.png"),
-  }
+	putInput := s3.PutObjectInput{
+		Bucket: aws.String("nicolasknoebber.com"),
+		Body:   reader,
+		Key:    aws.String("lambda-go-tree.png"),
+	}
 
-  result, err := svc.PutObject(&putInput)
-  if err != nil {
-    fmt.Println(err.Error)
-    return "", err
-  }
+	result, err := svc.PutObject(&putInput)
+	if err != nil {
+		fmt.Println(err.Error)
+		return "", err
+	}
 
 	return fmt.Sprintf("put object result: %s", result), nil
 }
 
-
 var dev = true
+
 func main() {
-  if !dev {
-    lambda.Start(HandleRequest)
-  }
-  draw()
+	// For testing locally
+	if dev {
+		createTree(TreeParam{LeftLength: 14, RightLength: 12, LeftAngle: 12, RightAngle: 45})
+		return
+	}
+	lambda.Start(HandleRequest)
 }
