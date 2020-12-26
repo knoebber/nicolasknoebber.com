1ccdb4f3f6a21613c623af30cb9a9ef0ebfb1e3d
Nicolas Knoebber <knoebber@gmail.com>
2020-12-26 11:31:24 -0800

    Consolidate style and use an auto sitemap for posts
    I removed the separate style sheet for posts so that all pages on the
    website look the same. It's much simpler now. Maybe less visually
    appealing but certainly easier to maintain. Hoping this will motivate
    me to write more. Next step is a adding a RSS feed.

--- src/site.el
+++ src/site.el
@@ -5,21 +5,16 @@
 
 ;;; Code:
 
-(defconst html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />")
+(defconst html-main-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />")
+(defconst html-posts-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\" />")
 
-(defconst html-main-head
-  (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"includes.css\" />" html-style))
+;; (defun postamble-text (text)
+;;   "Wraps TEXT in a span with class postamble-text."
+;;   (format "<span class=\"postamble-text\">%s</span>" text))
 
-(defconst html-posts-head
-  (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"../includes.css\" />" html-style))
-
-(defun postamble-text (text)
-  "Wraps TEXT in a span with class postamble-text."
-  (format "<span class=\"postamble-text\">%s</span>" text))
-
-(defun postamble-version (version)
-  "Wraps VERSION in a span with class version-number."
-  (format "<span class=\"postamble-text version-number\">%s</span>" version))
+;; (defun postamble-version (version)
+;;   "Wraps VERSION in a span with class version-number."
+;;   (format "<span class=\"postamble-text version-number\">%s</span>" version))
 
 (defun publish-nicolasknoebber-file ()
   "Exports current org file to html and uploads to s3://nicolasknoebber.com."
@@ -38,26 +33,64 @@
 
 (defconst html-postamble
   (concat
-   "<span id=\"made-with\">"
-   (postamble-text "powered by&nbsp;&nbsp;")
-   "<a href=\"https://www.gnu.org/software/emacs\">"
-   "<img src=\"../logo/emacs.svg\" id=\"emacs-logo\" alt=\"Emacs\">"
-   "</a>"
-   (postamble-version emacs-version)
-   "&nbsp<a href=\"https://orgmode.org\">"
-   "<img src=\"../logo/org-mode.svg\" id=\"org-mode-logo\" alt=\"Org\">"
-   "</a>"
-   (postamble-version org-version)
-   "</span>"
-   "<span id=\"published\">"
-   (format "%s" (format-time-string "%m/%e/%y"))
+   "<span id=\"made-with\">
+   exported by&nbsp;&nbsp;
+   <a href=\"https://www.gnu.org/software/emacs\"
+     ><img src=\"../logo/emacs.svg\" id=\"emacs-logo\" alt=\"Emacs\">
+   </a>"
+   emacs-version "&nbsp
+   <a href=\"https://orgmode.org\">
+     <img src=\"../logo/org-mode.svg\" id=\"org-mode-logo\" alt=\"Org\"></a>"
+   org-version
+   (format " on %s" (format-time-string "%m/%d/%y"))
    "</span>"))
+
 
 (defconst html-posts-postamble
   (concat
    html-postamble
-   "<noscript><div id=\"no-script-comment-message\">Enable scripts to see and post comments.</div></noscript>"
-   "<script type=\"text/javascript\" src=\"js/comments.js\"></script>"))
+   "
+<noscript>
+  <div id=\"no-script-comment-message\">Enable scripts to see and post comments.</div>
+</noscript>
+<script type=\"text/javascript\" src=\"js/comments.js\"></script>"))
+
+(defconst html-preamble "<a href=\"/\">Home</a>")
+(defconst html-posts-preamble
+  (concat html-preamble "<a href=\"/posts/\">Posts</a>"))
+
+  
+(defun generate-sitemap(title list)
+  "Default site map, as a string.
+TITLE is the title of the site map.  LIST is an internal
+representation for the files to include, as returned by
+`org-list-to-lisp'.  PROJECT is the current project.  This is
+almost identical to the version in the org publish source code.
+The only change I made is wrapping it in the .sitemap div."
+  (concat
+   "#+TITLE: " title
+   "\n\n"
+   "#+begin_sitemap\n"
+   (org-list-to-org list)
+   "\n#+end_sitemap"))
+
+(defun format-sitemap-entry (entry _style project)
+  "Format ENTRY in PROJECT."
+    (format "[[file:%s][%s]] =%s="
+	    entry
+	    (org-publish-find-title entry project)
+	    (format-time-string "%m/%d/%Y" (org-publish-find-date entry project))))
+
+(defun format-exported-timestamps(timestamp _backend _channel)
+  "Remove <> from exported org TIMESTAMP."
+  (print (replace-regexp-in-string "&[lg]t;" "" timestamp))
+  (replace-regexp-in-string "&[lg]t;" "" timestamp)
+)
+
+(eval-after-load 'ox
+  '(add-to-list
+    'org-export-filter-timestamp-functions
+    'format-exported-timestamps))
 
 (setq org-publish-project-alist
       `(("personal-website"
@@ -68,34 +101,29 @@
 	 :publishing-function org-html-publish-to-html
 	 :section-numbers nil
 	 :with-toc nil
-	 :with-title nil
 	 :html-head ,html-main-head
-	 :html-preamble "<a href=\"/\">Nicolas Knoebber</a>"
+	 :html-preamble ,html-preamble
 	 :html-postamble ,html-postamble
 	 :html-head-include-scripts nil
-	 :html-head-include-default-style nil)
+	 :html-head-include-default-style nil
+         )
 	("posts"
          :publishing-directory "~/projects/personal-website/posts"
 	 :base-directory "~/projects/personal-website/src/posts"
 	 :publishing-function org-html-publish-to-html
-	 :section-numbers nil
-	 :with-toc nil
 	 :html-head ,html-posts-head
-	 :html-head-include-scripts nil
-	 :html-head-include-default-style nil
-	 :html-preamble "<a href=\"../blog.html\">Blog</a>"
+         :html-head-include-scripts nil
+         :html-head-include-default-style nil
+	 :html-preamble ,html-posts-preamble
 	 :html-postamble ,html-posts-postamble
-	 )))
-
-
-(add-to-list 'org-export-filter-timestamp-functions
-             #'filter-timestamps)
-(defun filter-timestamps(data backend _channel)
-  "Remove <> around timestamps.  DATA is transcoded from the export BACKEND."
-  (replace-regexp-in-string "&[lg]t;" "" data))
-
-(setq-default org-display-custom-times t)
-(setq org-time-stamp-custom-formats
-      '("<%m/%d/%Y>" . "<%m/%d/%Y"))
+	 :auto-sitemap t
+         :sitemap-title "Blog"
+         :sitemap-function generate-sitemap
+         :sitemap-format-entry format-sitemap-entry
+         :sitemap-style list
+         :sitemap-sort-files anti-chronologically
+         :sitemap-filename "index.org"
+         )))
 
 ;;; site.el ends here
+
