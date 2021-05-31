65e3ab61544a4d09a7db30ed73a4810785f2f426
Nicolas Knoebber <knoebber@gmail.com>
2021-05-30 17:01:17 -0800

    Add ListPosts

--- lambda/microblog/microblog.go
+++ lambda/microblog/microblog.go
@@ -7,8 +7,10 @@ import (
 	"io"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
@@ -21,6 +23,25 @@ var (
 	ErrPostNotFound  = errors.New("post not found")
 	ErrImageNotFound = errors.New("image not found")
 )
+
+func ListPosts(sess *session.Session) ([]Post, error) {
+	var result []Post
+
+	db := dynamodb.New(sess)
+	output, err := db.Query(&dynamodb.QueryInput{
+		TableName: aws.String(tableName),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("listing posts: %w", err)
+	}
+
+	if err := dynamodbattribute.UnmarshalListOfMaps(output.Items, &result); err != nil {
+		return nil, fmt.Errorf("unmarshalling comments: %w", err)
+	}
+
+	return result, nil
+}
 
 func CreatePost(sess *session.Session, text string) (*Post, error) {
 	db := dynamodb.New(sess)
